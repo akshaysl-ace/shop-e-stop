41e73a34e904de55da4ebcc572ea4de0a05c9b93
akshaysl-ace <akshaysl3867@gmail.com>
2023-08-08 17:51:06 +0530

    mongodb setup

---
+++ backend/config/db.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const connectToDB = async () => {
+    try {
+        const connection = await mongoose.connect(process.env.MONGO_URI);
+        console.log("Connected to DB", connection.connection.host);
+    } catch (error) {
+        console.log("Error connecting DB -", error);
+        process.exit(1);
+    }
+}
+
+export default connectToDB;
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import products from './data/products.js';
 import dotenv from 'dotenv';
+import connectToDB from './config/db.js';
 
+// First of all, init the config else .env variables will not be loaded.
+// Do not remove below code or do not add anything above this line
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 
+connectToDB();
 const app = express();
 
 app.get('/', (req, res) => {
